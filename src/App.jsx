7f086fda865ba427051a662dd3c91ef07d99c0df
Aghakarim-23 <hemidzade1010@gmail.com>
2026-02-10 22:55:35 +0400

    removed Quiztimer from main files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import CreateQuestion from "./pages/CreateQuestion";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import QuizTimer from "./components/QuizTimer";
 import Results from "./pages/Results";
 
 const App = () => {
@@ -12,7 +11,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-question" element={<CreateQuestion />} />
-        <Route path="/quiz-timer" element={<QuizTimer />} />
         <Route path="/results" element={<Results />} />
       </Routes>
       
