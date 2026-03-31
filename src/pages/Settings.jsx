80b70c870f5db8874328dfe89aa0e14b8eecfb0f
Aghakarim-23 <hemidzade1010@gmail.com>
2026-03-31 15:19:08 +0400

    remove: darkMode

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,29 +3,14 @@ import useAuth from "../hooks/useAuth";
 import { Link } from "react-router-dom";
 
 const Settings = () => {
-  const [darkMode, setDarkMode] = useState(false);
   const { logout } = useAuth();
 
-  const handleToggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+
 
   return (
     <div className="bg-gray-100 min-h-screen p-4 md:p-8 flex justify-center">
       <div className="bg-white p-6 rounded-lg shadow-xl w-full max-w-md space-y-6">
         <h2 className="text-2xl font-bold text-center">Tənzimləmələr</h2>
-
-        <div className="flex items-center justify-between">
-          <span>Dark Mode</span>
-          <button
-            onClick={handleToggleDarkMode}
-            className={`px-4 py-2 rounded ${
-              darkMode ? "bg-gray-800 text-white" : "bg-gray-300 text-black"
-            } transition`}
-          >
-            {darkMode ? "On" : "Off"}
-          </button>
-        </div>
 
         <div className="flex flex-col gap-4 justify-center mt-4">
           <Link
