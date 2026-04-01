7e5e8dd901479f1a954620245c47e0f73c4f683d
Aghakarim-23 <hemidzade1010@gmail.com>
2026-04-02 03:48:43 +0400

    Add: progress and statistic section

--- src/components/home/ProgressAndSuccess.jsx
+++ src/components/home/ProgressAndSuccess.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FaRegUser } from "react-icons/fa";
 import { AiOutlineQuestionCircle, AiFillStar } from "react-icons/ai";
 import { MdCategory } from "react-icons/md";
@@ -12,14 +11,14 @@ const ProgressAndSuccess = () => {
       desc: "Üzvlər və istifadəçi statistikaları",
       icon: <FaRegUser size={30} className="text-blue-500" />,
       bg: "bg-blue-100",
-      value: 1200,
+      value: 42,
     },
     {
       title: "Ümumi suallar",
       desc: "Tez-tez verilən suallar",
       icon: <AiOutlineQuestionCircle size={30} className="text-green-500" />,
       bg: "bg-green-100",
-      value: 85,
+      value: 245,
     },
     {
       title: "Kateqoriyalar",
@@ -33,36 +32,36 @@ const ProgressAndSuccess = () => {
       desc: "Orta istifadəçi qiymətləndirilməsi",
       icon: <AiFillStar size={30} className="text-orange-500" />,
       bg: "bg-orange-100",
-      value: 4.8,
+      value: 4.5,
     },
   ];
 
-  // Intersection Observer
   const { ref, inView } = useInView({
-    triggerOnce: true, // bir dəfə animasiya
-    threshold: 0.3,    // 30% görünəndə başlasın
+    triggerOnce: true,
+    threshold: 0.3,
   });
 
   return (
-    <div className="min-h-screen flex justify-center items-center bg-blue-500 p-6">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-blue-50 p-6 bg-gradient-to-r from-blue-400 via-red-300 to-gray-400">
+      <h2 className="text-sm sm:text-base md:text-xl lg:text-2xl font-bold  text-center my-8">
+        Ümumi Performans və İrəliləyiş
+      </h2>
+
       <div
-        ref={ref} // burada observeri bağlayırıq
+        ref={ref}
         className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 w-full max-w-6xl"
       >
         {cards.map((card, idx) => (
           <div
             key={idx}
-            className={`${card.bg} rounded-lg shadow-lg p-6 flex flex-col items-center text-center`}
+            className={`${card.bg} rounded-lg shadow-lg p-6 flex flex-col items-center text-center mt-6
+             transform transition duration-300 hover:scale-105 hover:shadow-2xl`}
           >
             <div className="mb-4">{card.icon}</div>
             <h2 className="text-lg font-semibold mb-2">{card.title}</h2>
             <p className="text-gray-600 text-sm">{card.desc}</p>
             <p className="text-2xl font-bold mt-2">
-              {inView ? (
-                <CountUp end={card.value} duration={2} />
-              ) : (
-                0
-              )}
+              {inView ? <CountUp end={card.value} duration={2} /> : 0}
             </p>
           </div>
         ))}
