2400286abdf5d605b90393d9d3477eb143b110ee
Aghakarim-23 <hemidzade1010@gmail.com>
2026-04-04 14:31:13 +0400

    fix: form question for backend

--- src/pages/CreateQuestion.jsx
+++ src/pages/CreateQuestion.jsx
@@ -1,16 +1,14 @@
-import axios from "axios";
 import { useState } from "react";
 import { toast } from "react-toastify";
-import api from "../api/axios";
+// import api from "../api/axios";
 
 const CreateQuestion = () => {
   const [questionFormData, setQuestionFormData] = useState({
-    question: "",
-    optionA: "",
-    optionB: "",
-    optionC: "",
-    optionD: "",
-    optionE: "",
+    question: "Sual 1",
+    optionA: "cavab1 ",
+    optionB: "cavab2 ",
+    optionC: "cavab3 ",
+    optionD: "cavab4 ",
     correctAnswer: "",
   });
 
@@ -23,48 +21,60 @@ const CreateQuestion = () => {
   };
 
   const handleSubmit = async () => {
-
-  const payload = {
-  questionText: questionFormData.question,
-  options: [
-    questionFormData.optionA,
-    questionFormData.optionB,
-    questionFormData.optionC,
-    questionFormData.optionD,
-    questionFormData.optionE
-  ],
-  correctAnswer: questionFormData.correctAnswer
-};
+    if (
+      !questionFormData.question.trim() ||
+      !questionFormData.optionA.trim() ||
+      !questionFormData.optionB.trim() ||
+      !questionFormData.optionC.trim() ||
+      !questionFormData.optionD.trim() ||
+      questionFormData.correctAnswer === ""
+    ) {
+      toast.error("Bütün xanaları doldurun!");
+      return;
+    }
+    const payload = {
+      questionText: questionFormData.question,
+      options: [
+        questionFormData.optionA.trim(),
+        questionFormData.optionB.trim(),
+        questionFormData.optionC.trim(),
+        questionFormData.optionD.trim(),
+      ],
+      correctAnswer: Number(questionFormData.correctAnswer),
+    };
 
     try {
-        await api.post("/api/questions", payload)
-        toast.success("Question submitted successfully!");
-        setQuestionFormData({
-            question: "",
-            optionA: "",
-            optionB: "",
-            optionC: "",
-            optionD: "",
-            optionE: "",
-            correctAnswer: "",
-        })
+      // await api.post("/api/questions", payload)
+
+      toast.success("Question submitted successfully!");
+      setQuestionFormData({
+        question: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        optionD: "",
+        correctAnswer: "",
+      });
+      console.log("Payload", payload);
     } catch (error) {
-        console.error("Error submitting question:", error);
-        toast.error("Failed to submit question. Please try again." || error.message);
+      console.error("Error submitting question:", error);
+      toast.error(
+        "Failed to submit question. Please try again." || error.message,
+      );
     }
-  }
- 
+  };
+
   return (
     <>
       <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-3">
         <h1 className="text-2xl font-bold">Create a Question</h1>
-        <form 
-            onSubmit={(e) => {
-              e.preventDefault();
-              console.log(questionFormData);
-
-            }}
-            className="flex flex-col gap-2 bg-white p-6 rounded-md shadow-md w-full max-w-md mt-4">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+          className="flex flex-col gap-2 bg-white p-6 rounded-md shadow-md w-full max-w-md mt-4"
+        >
           <div className="flex flex-col gap-1">
             <label htmlFor="question">Question:</label>
             <input
@@ -78,7 +88,7 @@ const CreateQuestion = () => {
             />
           </div>
 
-          {["A", "B", "C", "D", "E"].map((option, index) => (
+          {["A", "B", "C", "D"].map((option, index) => (
             <div className="flex flex-col gap-1" key={index}>
               <label htmlFor={`option${option}`}>Option: {option}</label>
               <input
@@ -101,15 +111,15 @@ const CreateQuestion = () => {
             value={questionFormData.correctAnswer}
           >
             <option value="">Select Correct Answer</option>
-            {["A", "B", "C", "D", "E"].map((option, index) => (
-              <option key={index} value={option}>
+            {["A", "B", "C", "D"].map((option, index) => (
+              <option key={index} value={index}>
                 {option}
               </option>
             ))}
           </select>
-          <button 
-            onClick={handleSubmit}
-            className="w-full rounded-md bg-blue-500 active:bg-blue-600 cursor-pointer py-2 text-white">Submit</button>
+          <button className="w-full rounded-md bg-blue-500 active:bg-blue-600 cursor-pointer py-2 text-white">
+            Submit
+          </button>
         </form>
       </div>
     </>
